a4ffa96ff2d858febae1a94fd2197d2299661457
Lars <lars@inidus.com>
2018-04-24 17:22:11 +0100

    fix cloud build config

--- src/main/kotlin/hello/greeting/GreetingController.kt
+++ src/main/kotlin/hello/greeting/GreetingController.kt
@@ -11,6 +11,6 @@ class GreetingController {
 
     @GetMapping("/greeting")
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-            Greeting(counter.incrementAndGet(), "Hello... $name")
+            Greeting(counter.incrementAndGet(), "Helloooooooo, $name")
 
 }
